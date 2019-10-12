051a4e33457e10c00a442ebaf12b12030ca654f8
Akitektuo <alexcopindean@yahoo.com>
2019-10-12 23:31:05 +0300

    Add base for LC web app project

---
+++ 1st year/LC/base-computation/src/Calculator.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+
+class Calculator extends Component {
+  
+  componentWillMount() {
+    this.setState({
+    })
+  }
+
+  render() {
+    return (
+      <div className="Calculator">
+        
+      </div>
+    );
+  }
+}
+
+export default Calculator;
