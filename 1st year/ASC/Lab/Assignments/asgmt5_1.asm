10a1ef5613de22cc8ea908f21b41229ccef655b3
Akitektuo <alexcopindean@yahoo.com>
2019-11-11 21:02:03 +0200

    Add ASC changes

--- 1st year/ASC/Lab/Assignments/asgmt5_1.asm
+++ 1st year/ASC/Lab/Assignments/asgmt5_1.asm
@@ -8,28 +8,24 @@ import exit msvcrt.dll
 segment data use32 class=data
     a db 2, 1, -3, 3, 3, -4, 2, 6
     b db 4, 5, 7, 6, 2, 1
-    bLen equ $ - b
-    aLen equ $ - a
-    r resb len
+    bLen equ ($ - b) / 2
+    aLen equ ($ - a) / 2
+    r resb aLen + bLen
     
 segment code use32 class=code
 start:
-    mov esi, b ; i = 0 
-    mov ecx, bLen ; len = number of b elements
-    push_b: ; push all b values to the stack
-        mov al, [esi] ; al = b[i]
-        push al ; stack.push(al)
-        inc esi ; i++
-    loop push_b ; while (len > 0)
 
-    mov edi, r ; j = 0
+    mov esi, b ; i = 0
+    mov edi, r + bLen - 1 ; j = bLen - 1
     mov ecx, bLen ; len = number of b elements
-    pop_b: ; pop all b values from the stack to form the reverse array
-        pop al ; al = stack.pop()
-        mov [edi], al ; r[j] = al
-        inc edi ; j++
-    loop pop_b ; while (len > 0)
+    fill_b: ; obtain the reverse array of b
+        cld ; clear direction: ->->->
+        lods ; al = b[i++]
+        std ; set direction: <-<-<-
+        stosb ; r[j++] = al
+        loop fill_b ; whiile (len > 0)
     
+    cld ; clear direction: ->->->
     mov esi, a ; i = 0
     mov ecx, aLen ; len = number of a elements
     filter_a: ; copy all negative numbers from a
