414ed2f5dd3065a20f865144a958d7e27d60b28d
Akitektuo <alexcopindean@yahoo.com>
2020-03-13 09:27:01 +0200

    Add create test for service

--- 1st year/POO/Lab/Week3/TestService.c
+++ 1st year/POO/Lab/Week3/TestService.c
@@ -1,7 +1,16 @@
 #include "TestService.h"
 
-//TODO: continue tests
+void CreateService_SomeService_ServiceCreated()
+{
+	Service* testService = createService();
+
+	assert(testService->repository != NULL);
+	assert(testService->tracker != NULL);
+
+	destroyService(testService);
+}
 
 void runAllServiceTests()
 {
+	CreateService_SomeService_ServiceCreated();
 }
