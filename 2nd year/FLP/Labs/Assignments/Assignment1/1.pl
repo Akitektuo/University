781a83d269c9e96ad833d744398d18e3abf783f6
Akitektuo <alexcopindean@yahoo.com>
2020-10-22 10:26:19 +0300

    Correct implementations

--- 2nd year/FLP/Labs/Assignments/Assignment1/1.pl
+++ 2nd year/FLP/Labs/Assignments/Assignment1/1.pl
@@ -1,14 +1,30 @@
-isIncluded(Element, [Element | _]).
+%isIncluded(Element, l1...ln) = {
+%	false, n = 0
+%	true, Element = l1
+%	isIncluded(Element, l2..ln), otherwise
+%}
+  
+%isIncluded(Element: element, ListL list)
+%(I, I)
+%deterministic
+
+isIncluded(Element, [Element | _]) :- !.
 isIncluded(Element, [_ | Tail]) :- isIncluded(Element, Tail).
 
-intersect([], _, []).
-intersect([ListAHead | ListATail], ListB, Result) :-
-    \+ isIncluded(ListAHead, ListB), intersect(ListATail, ListB, Result).
+%intersect(l1...ln, q1...qm) = {
+%	[], n = 0
+%	l1 (+) intersect(l2...ln, q1...qm), isIncluded(l1, q1...qm)
+%	intersect(l2...ln, q1...qm), otherwise
+%}
+
+%intersect(List1: list, List2: list, Result: list)
+%(I, I, O)
+%deterministic
+
+intersect([], _, []) :- !.
 intersect([ListAHead | ListATail], ListB, [ListAHead | ResultTail]) :-
-    isIncluded(ListAHead, ListB), intersect(ListATail, ListB, ResultTail).
+    isIncluded(ListAHead, ListB), !, intersect(ListATail, ListB, ResultTail).
+intersect([_ | ListATail], ListB, Result) :-
+    intersect(ListATail, ListB, Result).
 
-
-
-
-
-
+%intersect([1, 3, 2, 7, 6], [2, 4, 7, 1], Result).
--- 2nd year/FLP/Labs/Assignments/Assignment1/2.pl
+++ 2nd year/FLP/Labs/Assignments/Assignment1/2.pl
@@ -1,5 +1,16 @@
-fill(Start, End, []) :- Start is End + 1.
+%fill(Start, End) {
+%	[], Start = End + 1
+%	Start (+) fill(Start + 1, End), otherwise
+%}
+
+%fill(Start: integer, End: integer, Result: list)
+%(I,I,O)
+%deterministic
+
+fill(Start, End, []) :- Start is End + 1, !.
 
 fill(Start, End, [Start | ResultTail]) :-
     NewStart is Start + 1,
     fill(NewStart, End, ResultTail).
+
+%fill(1, 10, Result).
