180969e4fc94747e423a461f448cacc92982435f
Akitektuo <alexcopindean@yahoo.com>
2020-10-26 21:40:42 +0200

    Add console interface

---
+++ 2nd year/APM/Assignments/Assignment2/src/utils/PredefinedProgramStates.java
@@ -0,0 +1,90 @@
+package utils;
+
+import model.ProgramState;
+import model.expression.ArithmeticExpression;
+import model.expression.ArithmeticOperator;
+import model.expression.ValueExpression;
+import model.expression.VariableExpression;
+import model.statement.*;
+import model.type.BooleanType;
+import model.type.NumberType;
+import model.value.BooleanValue;
+import model.value.IntegerValue;
+
+public class PredefinedProgramStates {
+    public static ProgramState PROGRAM_1;
+    public static ProgramState PROGRAM_2;
+    public static ProgramState PROGRAM_3;
+
+    public static final String VISUAL_PROGRAM_1 = new CodeFormatter("number v;")
+            .newLine()
+            .addLine("v = 2;")
+            .newLine()
+            .addLine("print(v);")
+            .build();
+    public static final String VISUAL_PROGRAM_2 = new CodeFormatter("number a;")
+            .addLine("number b;")
+            .newLine()
+            .addLine("a = 2 + 3 * 5;")
+            .addLine("b = a + 1;")
+            .newLine()
+            .addLine("print(b);")
+            .build();
+
+    public static final String VISUAL_PROGRAM_3 = new CodeFormatter("boolean a;")
+            .addLine("number v;")
+            .newLine()
+            .addLine("a = 2;")
+            .addLine("if (a)")
+            .addLine("v = 2;", 2)
+            .addLine("else")
+            .addLine("v = 3;", 2)
+            .newLine()
+            .addLine("print(v);")
+            .build();
+
+    static {
+        try {
+            PROGRAM_1 = new ProgramState(new CompoundStatement(
+                    new DeclarationStatement("v", new NumberType()),
+                    new AssignmentStatement("v", new ValueExpression(new IntegerValue(2))),
+                    new PrintStatement(new VariableExpression("v"))
+            ));
+
+            PROGRAM_2 = new ProgramState(new CompoundStatement(
+                    new DeclarationStatement("a", new NumberType()),
+                    new DeclarationStatement("b", new NumberType()),
+                    new AssignmentStatement("a", new ArithmeticExpression(
+                            new ValueExpression(new IntegerValue(2)),
+                            ArithmeticOperator.ADDITION,
+                            new ArithmeticExpression(
+                                    new ValueExpression(new IntegerValue(3)),
+                                    ArithmeticOperator.MULTIPLICATION,
+                                    new ValueExpression(new IntegerValue(5))
+                            )
+                    )),
+                    new AssignmentStatement("b", new ArithmeticExpression(
+                            new VariableExpression("a"),
+                            ArithmeticOperator.ADDITION,
+                            new ValueExpression(new IntegerValue(1))
+                    )),
+                    new PrintStatement(new VariableExpression("b"))
+            ));
+
+            PROGRAM_3 = new ProgramState(new CompoundStatement(
+                    new DeclarationStatement("a", new BooleanType()),
+                    new DeclarationStatement("v", new NumberType()),
+                    new AssignmentStatement("a", new ValueExpression(new BooleanValue(true))),
+                    new IfStatement(
+                            new VariableExpression("a"),
+                            new AssignmentStatement("v", new ValueExpression(new IntegerValue(2))),
+                            new AssignmentStatement("v", new ValueExpression(new IntegerValue(3)))
+                    ),
+                    new PrintStatement(new VariableExpression("v"))
+            ));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- 2nd year/APM/Assignments/Assignment2/src/view/MainView.java
+++ 2nd year/APM/Assignments/Assignment2/src/view/MainView.java
@@ -1,35 +1,67 @@
 package view;
 
 import controller.Controller;
-import model.ProgramState;
-import model.expression.ValueExpression;
-import model.expression.VariableExpression;
-import model.statement.AssignmentStatement;
-import model.statement.CompoundStatement;
-import model.statement.DeclarationStatement;
-import model.statement.PrintStatement;
-import model.type.NumberType;
-import model.value.IntegerValue;
+import model.expression.ExpressionException;
+import model.statement.StatementException;
 import repository.Repository;
+import utils.CodeFormatter;
+
+import java.util.Scanner;
+
+import static utils.PredefinedProgramStates.*;
 
 public class MainView {
     private final Controller controller;
+    private final Scanner input = new Scanner(System.in);
 
     public MainView(Repository repository) {
         controller = new Controller(repository, true);
     }
 
     public void start() {
-        try {
-            controller.addProgramState(new ProgramState(new CompoundStatement(
-                    new DeclarationStatement("n", new NumberType()),
-                    new AssignmentStatement("n", new ValueExpression(new IntegerValue(2))),
-                    new PrintStatement(new VariableExpression("n"))
-            )));
-            System.out.println("Final output: " + controller.executeAllSteps());
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
+        System.out.println("\nWelcome to TOY LANGUAGE INTERPRETER! (version 132.202010262100-beta)");
+
+        while (true) {
+            printMenu();
+            var option = readOption();
+
+            try {
+                controller.addProgramState(switch (option) {
+                    case 1 -> PROGRAM_1;
+                    case 2 -> PROGRAM_2;
+                    case 3 -> PROGRAM_3;
+                    default -> throw new IllegalStateException("Unexpected value: " + option);
+                });
+            } catch (Exception ignore) {
+                break;
+            }
+
+            try {
+                System.out.println("Final output: " + controller.executeAllSteps());
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
         }
+
+        System.out.println("Closing program...");
     }
 
+    private void printMenu() {
+        System.out.println("Choose an option from below:");
+        System.out.println("0. Exit");
+
+        System.out.println("\n1. Execute:");
+        System.out.println(VISUAL_PROGRAM_1);
+
+        System.out.println("\n2. Execute:");
+        System.out.println(VISUAL_PROGRAM_2);
+
+        System.out.println("\n3. Execute:");
+        System.out.println(VISUAL_PROGRAM_3);
+    }
+
+    private int readOption() {
+        System.out.print("> ");
+        return input.nextInt();
+    }
 }
