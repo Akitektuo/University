2f3540b12ede517dc45c4402f7d32aa0056cbfd9
Akitektuo <alexcopindean@yahoo.com>
2021-02-27 21:59:42 +0200

    Add DBS lab and start for AI assignment

---
+++ 2nd year/AI/Assignments/Assignment1/constants.py
@@ -0,0 +1,15 @@
+class Color:
+    BLUE = (0, 0, 255)
+    GRAY_BLUE = (50, 120, 120)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+
+
+class Direction:
+    UP = 0
+    LEFT = 1
+    DOWN = 2
+    RIGHT = 3
+    DIRECTIONS = [[-1, 0], [1, 0], [0, 1], [0, -1]]
