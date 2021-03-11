b9fe53ee6feb907e98e36020c01dc6c0cbcf1822
Akitektuo <alexcopindean@yahoo.com>
2021-03-11 12:53:52 +0200

    Add DBS lab 2

---
+++ 2nd year/DBS/Assignments/WindowsForm/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace WindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridUsers = new System.Windows.Forms.DataGridView();
+            this.dataGridLogs = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonUpdateDB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridUsers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridLogs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridUsers
+            // 
+            this.dataGridUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridUsers.Location = new System.Drawing.Point(41, 30);
+            this.dataGridUsers.Name = "dataGridUsers";
+            this.dataGridUsers.RowTemplate.Height = 25;
+            this.dataGridUsers.Size = new System.Drawing.Size(490, 127);
+            this.dataGridUsers.TabIndex = 0;
+            // 
+            // dataGridLogs
+            // 
+            this.dataGridLogs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridLogs.Location = new System.Drawing.Point(41, 195);
+            this.dataGridLogs.Name = "dataGridLogs";
+            this.dataGridLogs.RowTemplate.Height = 25;
+            this.dataGridLogs.Size = new System.Drawing.Size(490, 116);
+            this.dataGridLogs.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pirates";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ships";
+            // 
+            // buttonUpdateDB
+            // 
+            this.buttonUpdateDB.Location = new System.Drawing.Point(590, 158);
+            this.buttonUpdateDB.Name = "buttonUpdateDB";
+            this.buttonUpdateDB.Size = new System.Drawing.Size(101, 34);
+            this.buttonUpdateDB.TabIndex = 4;
+            this.buttonUpdateDB.Text = "Update DB";
+            this.buttonUpdateDB.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 583);
+            this.Controls.Add(this.buttonUpdateDB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridLogs);
+            this.Controls.Add(this.dataGridUsers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridUsers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridLogs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridUsers;
+        private System.Windows.Forms.DataGridView dataGridLogs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonUpdateDB;
+    }
+}
+
---
+++ 2nd year/DBS/Assignments/WindowsForm/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        private readonly string USERS_TABLE = "Users";
+        private readonly string LOGS_TABLE = "Logs";
+
+        private DataSet dataSet = new DataSet();
+        private SqlConnection dbConnection;
+
+        private SqlDataAdapter dataAdapterUsers, dataAdapterLogs;
+        private BindingSource bindingUsers, bindingLogs;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dbConnection = new SqlConnection("Data Source = DESKTOP-K0E4MHB\\SQLEXPRESS; " +
+                "Initial Catalog = Expenses; Integrated Security = SSPI;");
+
+            dataAdapterUsers = new SqlDataAdapter($"SELECT * FROM {USERS_TABLE}", dbConnection);
+            dataAdapterLogs = new SqlDataAdapter($"SELECT * FROM {LOGS_TABLE}", dbConnection);
+
+            dataAdapterUsers.Fill(dataSet, USERS_TABLE);
+            dataAdapterLogs.Fill(dataSet, LOGS_TABLE);
+
+            var dataRelation = new DataRelation(
+                "FK__Logs__UserId__3F466844",
+                dataSet.Tables[USERS_TABLE].Columns["Id"],
+                dataSet.Tables[LOGS_TABLE].Columns["UserId"]);
+            dataSet.Relations.Add(dataRelation);
+
+            bindingUsers = new BindingSource();
+            bindingUsers.DataSource = dataSet;
+            bindingUsers.DataMember = USERS_TABLE;
+
+            bindingLogs = new BindingSource();
+            bindingLogs.DataSource = bindingUsers;
+            bindingLogs.DataMember = "FK__Logs__UserId__3F466844";
+
+            dataGridUsers.DataSource = bindingUsers;
+            dataGridLogs.DataSource = bindingLogs;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
