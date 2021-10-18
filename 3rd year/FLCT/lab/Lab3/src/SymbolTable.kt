a96a27818f69098e5832dfb2dc50ab5f8f46f7dc
Alex Copindean <alexcopindean@yahoo.com>
2021-10-18 16:48:52 +0300

    Clean code

--- 3rd year/FLCT/lab/Lab3/src/SymbolTable.kt
+++ 3rd year/FLCT/lab/Lab3/src/SymbolTable.kt
@@ -3,18 +3,17 @@ class SymbolTable(private var capacity: Int = 17, private val loadFactor: Float 
     private var size = 0
 
     fun add(token: String): Pair<Int, Int> {
-        val tokenPosition = find(token)
-        if (tokenPosition != null) {
-            return tokenPosition
+        find(token)?.let {
+            return it
         }
 
         growIfLoadFactorExceeded()
         size++
         val hashKey = token.hash()
-        val position = map[hashKey].size
+        val tokenPosition = map[hashKey].size
         map[hashKey].add(token)
 
-        return hashKey to position
+        return hashKey to tokenPosition
     }
 
     fun find(token: String): Pair<Int, Int>? {
