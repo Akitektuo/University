3250cb7759f3eeca9811395bc71963871808233c
Alex Copindean <alexcopindean@yahoo.com>
2021-11-01 21:01:05 +0200

    Add base for PDP lab 5

---
+++ 3rd year/FLCT/lab/Lab5/Parser/BaseParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.Parsers
+{
+    abstract class BaseParser
+    {
+        protected List<string> Urls { get; set; }
+
+        protected BaseParser(List<string> urls)
+        {
+            Urls = urls;
+        }
+    }
+}
---
+++ 3rd year/FLCT/lab/Lab5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab5
+{
+    class Program
+    {
+        private readonly List<string> URLS = new() {
+            "www.cs.ubbcluj.ro/~motogna/LFTC",
+            "www.cs.ubbcluj.ro/~rlupsa/edu/pdp/",
+            "www.cs.ubbcluj.ro/~forest"
+        };
+
+        static void Main()
+        {
+
+        }
+    }
+}
